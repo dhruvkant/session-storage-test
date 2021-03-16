b5592a840746846df24887a15f2a8ace5ec062dc
dhruvkant <dhruvkant1@gmail.com>
2021-03-16 12:06:57 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule, 
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
