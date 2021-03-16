3ce1f6d02065bb89d79dcb89bc76269ce87123af
dhruvkant <dhruvkant1@gmail.com>
2021-03-16 11:44:15 +0530

    5 files modified

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SigninComponent } from "./signin/signin.component";
 import { LoginRoutingModule } from "./login-routing.module";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule, LoginRoutingModule],
   declarations: [SigninComponent]
 })
 export class LoginModule {}
